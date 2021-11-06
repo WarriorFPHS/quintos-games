8f376db97ea1845f99927a7f3b98df23bcc0a69b
WarriorFPHS <93843094+WarriorFPHS@users.noreply.github.com>
2021-11-06 14:50:05 -0700

    Initial commit

---
+++ load.js
@@ -0,0 +1,4 @@
+QuintOS.username = 'your-username'; // put your github username!
+QuintOS.level = 1; // the type of computer to use
+QuintOS.dir = '.'; // the folder with the game folder in it
+QuintOS.gameSelect = 'HelloWorld'; // load test program
