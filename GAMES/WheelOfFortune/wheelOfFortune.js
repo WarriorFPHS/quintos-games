1232953199797443667a957134162d3d1a8d380c
WarriorFPHS <aarnavg2007@gmail.com>
2022-05-05 17:05:21 -0700

    1.1.5

--- GAMES/WheelOfFortune/wheelOfFortune.js
+++ GAMES/WheelOfFortune/wheelOfFortune.js
@@ -1,27 +1,36 @@
-// the category is "Fun and Games"
-let phrasesList =
-	"Tourist-Friendly Destination|Skateboarding & Snowboarding|Underground Entertainment|Competitive Shuffleboard|Cookie-Decorating Contest|Grand-Opening Celebration|Multicultural Activities|Neighborhood Get-Together|Photography & Scrapbooking|Skateboards & Rollerblades|Snowball-Throwing Contest|Snowshoeing & Snowboarding|International Carnivals|Performing-Arts Festival|Skateboard & Rollerblades|Spur-Of-The-Moment Getaway|Unforgettable Carnivals|Wholesome Entertainment|Afterschool Activities|Arts-And-Crafts Festival|Disappearing-Coin Trick|Ice-Carving Competition|Mixologist Competition|Snowbiking & Airboarding|Big-Ticket Attractions|City-Center Horseraces|Halloween Festivities|Hamletscenen Festival|Helium-Filled Balloons|Lion-Dance Competition|Mountaineering & Skiing|Snowboarding & Sledding|Spontaneous Nightlife|Traditional Macaroons|Battleship Destroyer|Building Sandcastles|Carnival Attractions|Festive Celebrations|Fingerprint Drawings|Fingerprints Drawing|Gingerbread-House Kit|Going Paddleboarding|Handball & Racquetball|Helium-Filled Baloons|Curling Championship|In-The-Kitchen Puzzles|Jack-O'-Lantern Carving|Model-Airplane Racing|Renaissance Festival|Alpine Snowboarding|Charades & Pictionary|Contestant Searches|Cross-Country Skiing|Festive Celebration|Five-Gallon Stockpot|Freestyle Wrestling|Frisbee Competition|Interactive Puzzles|Marshmallow Animals|Mini-Golf Tournament|Murder-Mystery Party|Playing Racquetball|Rhythmic Gymnastics|Roller-Coaster Rides|Sled-Pulling Contest|Sleight-Of-Hand Magic|Yuletide Activities|Back-Road Bicycling|Balance-Beam Tricks|Complicated Puzzle|Computer Solitaire|Fast-Pitch Softball|Festive Activities|Freshwater Fishing|Gymnastics Routine|Outdoor Recreation|Playful Activities|Playing Backgammon|Playing Horseshoes|Playing Pictionary|Ski Mountaineering|Slight-Of-Hand Magic|Table Shuffleboard|Water-Balloon Fight|Water-Balloon Throw|Weeklong Festivals|Baseball & Softball|Chess Competition|Collectible Dolls|Crossword Puzzles|Cultural Festival|Equestrian Sports|Filmmaking Genius|Football & Baseball|Football Practice|Going Parasailing|Goldfish Scooping|Halloween Hayride|Hot-Air Ballooning|Indoor Volleyball|Jovial Sing-Alongs|Juggling Beanbags|Late-Night Hayride|Medieval Festival|Miniature Golfing|Playing Asteroids|Playing Badminton|Playing Hopscotch|Playing Paintball|Playing Solitaire|Playing Tic-Tac-Toe|Pothole Exploring|Riverside Camping|Softball & Baseball|Tag-Team Wrestling|Two-Story Carousel|Water-Balloon Toss|Wheelbarrow Races|Writing Limericks|Adventure Racing|Alphabet Magnets|Ballroom Dancing|Barbeque Bonanza|Beach Volleyball|Biggie Boardings|Childhood Heroes|Chinese Checkers|Christmas Crafts|Crossword Puzzle|Demolition Derby|Disappearing Ink|Doing Handstands|Double-Coin Trick|Dungeons & Dragons|Fantasy Football|Fraternity Prank|Freestyle Skiing|Fun Brainteasers|Going Spelunking|Gorgeous Fishing|Gorgeous Golfing|Habanos Festival|Headband Antlers|Hula Competition|Hula-Hoop Contest|Indoor Go-Carting|Indoor Go-Karting|Inflatable Slide|Interactive Toys|Japanese Archery|Juggling Oranges|Knock-Knock Jokes|Masquerade Balls|Narrated Cruises|Paper Snowflakes|Ping-Pong Paddles|Playing Checkers|Playing Dominoes|Playing Jeopardy|Playing Jeopardy!|Playing Kickball|Playing Lacrosse|Playing Monopoly|Playing Peekaboo|Playing Ping-Pong|Playing Scrabble|Popcorn Garlands|Potato-Sack Races|Renaissance Fair|Riding Piggyback|Shooting Marbles|Spitting Contest|Sprint-Car Racing|Street Carnivals|Swim-Up Blackjack|Twenty Questions|Ultimate Frisbee|Urban Spelunking|Volleyball Match|Wheelbarrow Race|Winter Carnivals|Amazing History|Amusement Rides|Anderlecht Fair|Balloon Animals|Big-Wave Surfing|Board-Game Night|Boggle & Scrabble|Boogie Boarding|Burping Contest|Classic Yahtzee|Community Chest|Confetti Cannon|Country Dancing|Cricket & Croquet|Croquet Mallets|Deep-Sea Fishing|Downhill Skiing|Downhill Slalom|Dragon-Boat Race|Exciting Rounds|Family Cookouts|Finger Painting|Gaelic Football";
-
 /* Make an array of phrases, pick a random phrase, and split pharse into an array of words */
 let score = 0;
-let stopAddingLetters = false;
-let phrases = phrasesList.split('|');
-let num = Math.floor(Math.random() * phrases.length);
-let phrase = phrases[num];
-let words = phrase.split(' ');
-log(phrase);
+let stopAddingLetters;
+let phrases;
+let phrase;
+let words;
+let le;
+let count;
 
-/* Make a board array to represent the letters in the phrase */
-// phrase -> ['Community', 'Chest']
-// board -> [
-//   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-//   [' ', ' ', ' ', ' ', ' ']
-// ]
-let board = [];
-for (let i = 0; i < words.length; i++) {
-	board.push(' '.repeat(words[i].length).split(''));
+async function setupGame() {
+	let filePath = QuintOS.dir + '/phrases.txt';
+	let data = await fetch(filePath);
+	let phrasesList = await data.text();
+	phrases = phrasesList.split('\r\n');
+	newGame();
 }
-log(board);
+setupGame();
+
+async function newGame() {
+	await eraseRect(1, 1, 38, 13);
+	stopAddingLetters = false;
+	let num = Math.floor(Math.random() * phrases.length);
+	log(phrases);
+	phrase = phrases[num];
+	phrases.splice(num, 1);
+	log(phrases);
+	words = phrase.split(' ');
+	log(phrase);
+	le = phrase.length - words.length;
+	count = 0;
+	displayBoxes();
+	addLetter();
+}
 
 /* Display all the boxes for the phrase */
 function displayBoxes() {
@@ -32,31 +41,43 @@ function displayBoxes() {
 		}
 	}
 }
-displayBoxes();
 
 /* Add a letter to a random empty box */
 async function addLetter() {
-	for (let a = 0; a < phrase.length - words.length + 1; a++) {
-		let letter = ' ';
-		let i;
-		let j;
-		while (letter == ' ') {
-			i = Math.floor(Math.random() * words.length);
-			j = Math.floor(Math.random() * words[i].length);
-			letter = words[i][j];
-		}
-		log(letter, j);
-		text(letter, i * 4 + 3, j * 3 + 3);
-		let word = words[i];
-		words[i] = word.slice(0, j) + ' ' + word.slice(j + 1);
-		log(words);
-		await delay(1000);
-		if (stopAddingLetters == true) {
-			return;
-		}
+	let letter = ' ';
+	let i;
+	let j;
+
+	log(count);
+	log(le);
+	// test to see if the letter chooosen is a space
+	while (letter == ' ') {
+		i = Math.floor(Math.random() * words.length);
+		j = Math.floor(Math.random() * words[i].length);
+		letter = words[i][j];
 	}
+	log(letter, j);
+	text(letter, i * 4 + 3, j * 3 + 3);
+
+	// replace letter with a space in the array of words (in the phrase)
+	let word = words[i];
+	words[i] = word.slice(0, j) + ' ' + word.slice(j + 1);
+	log(words);
+
+	count++;
+	if (count > le) {
+		await alert('Too slow!', 18, 2);
+		score = score - 3;
+		return;
+	}
+	await delay(1000);
+	if (stopAddingLetters == true) {
+		return;
+	}
+	addLetter();
 }
-addLetter();
+
+text('Score:' + score, 14, 2);
 let bigBuzzer = `
 |⎺|__  _   _ ___________ _ __
 | '_ \\| | | |_  /_  / _ \\ '__|
@@ -66,12 +87,15 @@ let bigBuzzer = `
 async function buzz() {
 	stopAddingLetters = true;
 	let answer = await prompt('Guess the phrase', 18, 2);
-	if (answer == phrase) {
-		await alert('You won!', 18, 2);
-		score = score + lettersleft;
-		newgame();
+	if (answer.toLowerCase() == phrase.toLowerCase()) {
+		await alert('You got it!', 18, 2);
+
+		score = score + (le - count);
+		text('Score:' + score + '      ', 14, 2);
+		newGame();
 	} else {
 		score = score - 1;
+		text('Score:' + score + '      ', 14, 2);
 		stopAddingLetters = false;
 		addLetter();
 	}
