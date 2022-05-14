28a320411851ecfaa2557a0f4d25033416745f46
WarriorFPHS <aarnavg2007@gmail.com>
2022-05-14 15:17:50 -0700

    1.1.6

--- load.js
+++ load.js
@@ -1,3 +1,3 @@
 QuintOS.user = 'WarriorFPHS'; // put your github username!
 QuintOS.dir = 'GAMES'; // the folder with the game folder in it
-QuintOS.game = 'WheelOfFortune'; // load test program
+QuintOS.game = 'WorldWideWeb'; // load test program
